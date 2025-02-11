05085892606afb999f737b90fdc2e83ae67f94cb
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:33:41 +0400

    Models folder
    Models folder

---
+++ Rating.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviaCatalogiAPI.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+
+        [Required, Range(1, 5)]
+        public byte Stars {  get; set; }
+
+        [Required, MinLength(150), MaxLength(250)]
+        public string? RateText { get; set; }
+    }
+}
