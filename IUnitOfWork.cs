3744c7635f2f5e4f5d2c9bd4a16051e1291fda59
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:36:13 +0400

    Repositories

---
+++ IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using MovieCatalogWebService.Repositories;
+using System.Threading.Tasks;
+
+namespace MoviaCatalogiAPI.Repositories
+{
+    public interface IUnitOfWork
+    {
+        IMovieRepository MovieRepository { get; }
+        IRatingRepository RatingRepository { get; }
+        Task SaveAsync();
+    }
+}
+
