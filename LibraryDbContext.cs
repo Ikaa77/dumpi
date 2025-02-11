10e44891982285caa3514b6e26c12da658a8a677
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:30:00 +0400

    Data folder
    Data folder

---
+++ LibraryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviaCatalogiAPI.Models;
+
+namespace MoviaCatalogiAPI.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+    }
+}
+
+
