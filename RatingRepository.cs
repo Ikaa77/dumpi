5455f211834f295daf63d96593ab07cc47db735d
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:40:59 +0400

    Repositories

---
+++ RatingRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviaCatalogiAPI.Data;
+using MoviaCatalogiAPI.Models;
+using MovieCatalogWebService.Repositories;  // Ensure this is included
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MoviaCatalogiAPI.Repositories
+{
+    public class RatingRepository : IRatingRepository
+    {
+        private readonly LibraryDbContext _context;
+
+        public RatingRepository(LibraryDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all ratings for a specific movie
+        public async Task<IEnumerable<Rating>> GetRatingsByMovieIdAsync(int movieId)
+        {
+            return await _context.Ratings
+                .Where(r => r.MovieId == movieId)
+                .ToListAsync();
+        }
+
+        // Add a new rating
+        public async Task AddRatingAsync(Rating rating)
+        {
+            await _context.Ratings.AddAsync(rating);
+        }
+    }
+}
