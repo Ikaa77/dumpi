aaca5d687b85742e15010691fc7e84d89b23c4ca
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:33:06 +0400

    Models folder
    models folder

---
+++ Movie.cs
@@ -0,0 +1,25 @@
+﻿using MoviaCatalogiAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviaCatalogiAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required, MinLength(2), MaxLength(150)]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Genre { get; set; }
+
+        [Required]
+        public int ReleaseYear { get; set; }
+
+        [Required, MinLength(15), MaxLength(150)]
+        public string? Country { get; set; }
+
+        [Required, MinLength(250)]
+        public string? Description { get; set; }
+    }
+}
