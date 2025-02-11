40fc845b8a6b59f33998407a0b129e3dae4b539d
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:38:48 +0400

    Repositories

---
+++ UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using MoviaCatalogiAPI.Data;
+using MovieCatalogWebService.Repositories;  // Add the correct namespace here
+using System.Threading.Tasks;
+
+namespace MoviaCatalogiAPI.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LibraryDbContext _context;
+
+        public IMovieRepository MovieRepository { get; }
+        public IRatingRepository RatingRepository { get; }
+
+        public UnitOfWork(LibraryDbContext context, IMovieRepository movieRepository, IRatingRepository ratingRepository)
+        {
+            _context = context;
+            MovieRepository = movieRepository;
+            RatingRepository = ratingRepository;
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
