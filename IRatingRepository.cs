9f56c977d2e326c403c537ece7d2e5b89157abe7
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:35:48 +0400

    Repositories

---
+++ IRatingRepository.cs
@@ -0,0 +1,13 @@
+﻿using MoviaCatalogiAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MovieCatalogWebService.Repositories
+{
+    public interface IRatingRepository
+    {
+        Task<IEnumerable<Rating>> GetRatingsByMovieIdAsync(int movieId);
+        Task AddRatingAsync(Rating rating);
+    }
+}
+
