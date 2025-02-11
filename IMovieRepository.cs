6fa08b8c8f00431764cde9656d7123a8d2c2e2a3
Ikaa77 <149305213+Ikaa77@users.noreply.github.com>
2025-02-11 23:34:53 +0400

    Repositories

---
+++ IMovieRepository.cs
@@ -0,0 +1,15 @@
+﻿using MoviaCatalogiAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MovieCatalogWebService.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetAllMoviesAsync();
+        Task<Movie?> GetMovieByIdAsync(int id);
+        Task AddMovieAsync(Movie movie);  // Added method for adding movie
+        Task UpdateMovieAsync(Movie movie);
+        Task DeleteMovieAsync(int id);
+    }
+}
